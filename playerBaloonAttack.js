b7f0ceade52aaa718ae5fbc5626c890650b04600
chrosslaw <Chrosslaw@gmail.com>
2022-07-07 13:37:56 -0500

    baloonAttack solution

--- playerBaloonAttack.js
+++ playerBaloonAttack.js
@@ -23,3 +23,19 @@ class Player {
     console.log(`Player: ${this.name} -- Balloons Left: ${this.balloonCount}`);
   }
 }
+balloonAttack = (p1, p2) => {
+  const p1bc = (p1.balloonCount -= p2.hitsPerMinute * 10);
+  const p2bc = (p2.balloonCount -= p1.hitsPerMinute * 10);
+  p1.status();
+  p2.status();
+  console.log(`Winner:`);
+  return p1bc == p2bc
+    ? "Tie"
+    : p1.balloonCount > p2.balloonCount
+    ? p1.name
+    : p2.name;
+};
+
+const p1 = new Player("Balloon Destroyer", 5);
+const p2 = new Player("Eagle Eye", 8);
+console.log(balloonAttack(p1, p2));
